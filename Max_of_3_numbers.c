69d71aff762482f0a0f57c540d819d92d98a2761
everydayphilos4 <88627802+everydayphilos4@users.noreply.github.com>
2023-08-04 18:18:39 +0530

    Create Max_of_3_numbers.c

---
+++ Max_of_3_numbers.c
@@ -0,0 +1,22 @@
+// Max of 3 numbers
+#include <stdio.h>
+int main(){
+    int a,b,c,max;
+    printf("enter a : ");
+    scanf("%d",&a);
+    printf("enter b : ");
+    scanf("%d",&b);
+    printf("enter c : ");
+    scanf("%d",&c);
+
+    if(a>b && a>c)
+         max = a;
+    else
+        if(b>c)
+            max = b;
+    else
+            max = c;    
+
+    printf("maximum number = %d",max); 
+    return 0;    
+}
