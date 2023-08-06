e371e4c4521a2e63595f74509b472448b87bd6dc
everydayphilos4 <88627802+everydayphilos4@users.noreply.github.com>
2023-08-06 12:50:58 +0530

    Create first_n_natural_numbers.c

---
+++ first_n_natural_numbers.c
@@ -0,0 +1,12 @@
+//print first n mumbers
+#include <stdio.h>
+int main(){
+
+    int n,i = 1;
+    printf("enter a number : ");
+    scanf("%d",&n);
+    for(i;i<=n;i++){
+        printf("%d\n",i);
+    }
+    return 0;    
+}
