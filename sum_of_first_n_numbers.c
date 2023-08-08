71e88a5b14bded4b19d50dc08c61ec6828c6692e
everydayphilos4 <88627802+everydayphilos4@users.noreply.github.com>
2023-08-08 08:13:02 +0530

    Create sum_of_first_n_numbers.c

---
+++ sum_of_first_n_numbers.c
@@ -0,0 +1,14 @@
+// print sum of  first n mumbers
+#include <stdio.h>
+int main(){
+
+    int n,sum = 0,i = 1;
+    printf("enter a number : ");
+    scanf("%d",&n);
+    for(i;i<=n;i++){
+        printf("%d\n",i);
+        sum = sum + i;
+    }
+        printf("sum = : %d\n",sum);
+    return 0;    
+}
