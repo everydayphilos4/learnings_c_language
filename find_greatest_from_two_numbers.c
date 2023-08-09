35e7e0fea6f4c6ff514eba4bf93116fad380019a
everydayphilos4 <88627802+everydayphilos4@users.noreply.github.com>
2023-08-09 10:52:19 +0530

    Create find_greatest_from_two_numbers.c

---
+++ find_greatest_from_two_numbers.c
@@ -0,0 +1,14 @@
+// find greatest from two numbers
+#include <stdio.h>
+int main(){
+
+    int a,b;
+    printf("enter the numbers : ");
+    scanf("%d\n%d",&a,&b);
+    if((a-b) < 0){
+        printf("greatest number is : %d",b);
+    }
+    else printf("greatest number is : %d",a);
+      
+    return 0;    
+}
