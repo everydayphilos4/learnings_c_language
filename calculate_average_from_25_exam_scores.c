8bc6fa10e8f7fc7ad7188d3d121061dcb808b13c
everydayphilos4 <88627802+everydayphilos4@users.noreply.github.com>
2023-08-09 10:53:54 +0530

    Create calculate_average_from_25_exam_scores.c

---
+++ calculate_average_from_25_exam_scores.c
@@ -0,0 +1,16 @@
+// calculate average from 25 exam scores
+#include<stdio.h>
+int main(){
+
+    int i,marks,sum = 0;
+
+    for(i = 1;i<=25;i++){
+        printf("enter marks of subject number %d : ",i);
+        scanf("\n%d",&marks);
+        sum = sum + marks;
+    }
+    float average = sum / 25;
+    printf("average of 25 subjects is : %f ",average);
+
+    return 0;
+}
