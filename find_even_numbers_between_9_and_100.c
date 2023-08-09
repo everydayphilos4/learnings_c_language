96ad16b9d5d461cf074c86251ac9a905b65cb684
everydayphilos4 <88627802+everydayphilos4@users.noreply.github.com>
2023-08-09 10:53:17 +0530

    Create find_even_numbers_between_9_and_100.c

---
+++ find_even_numbers_between_9_and_100.c
@@ -0,0 +1,23 @@
+// find even numbers between 9 and 100.
+#include <stdio.h>
+int main(){
+
+    int a;
+    printf("enter the number between 9 and 100 : ");
+    scanf("%d",&a);
+
+    if(a%2 == 0){
+        printf("%d is an even number \n ",a);
+    }
+    else printf("%d is an odd number \n",a);
+
+    printf(" list of all even numbers between 9 and 100 ");
+    for(int i=9;i<100;i++){
+        if(i%2 == 0){
+        printf("%d is an even number \n",i);
+    }else
+        printf("%d is an odd number \n",i);
+    }
+
+    return 0;    
+}
